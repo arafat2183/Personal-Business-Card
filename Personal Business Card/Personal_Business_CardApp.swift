8ccde0a56ee816128485d4422c65796a61568d28
arafat2183 <akash076987@gmail.com>
2023-09-22 17:51:11 +0900

    Initial commit

---
+++ Personal Business Card/Personal_Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Business_CardApp.swift
+//  Personal Business Card
+//
+//  Created by Akash Arafat on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
